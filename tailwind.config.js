f9e708768477faba70da3d2bfb3a5da1366c76ef
phuochoan <89370644+PhuocHoan@users.noreply.github.com>
2024-10-21 21:52:22 +0700

    finish styling for landing page, not yet responsive

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+        "./*.{html,js}", // Add the path to your HTML file
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
